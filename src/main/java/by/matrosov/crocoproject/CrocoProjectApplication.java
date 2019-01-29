0d3d1e6172cdd50c2d1e5e8f28d2f8d24f4c5327
Andrey Matrosov <matrosovs2008@yandex.ru>
2019-01-29 19:04:17 +0300

    first commit

---
+++ src/main/java/by/matrosov/crocoproject/CrocoProjectApplication.java
@@ -0,0 +1,14 @@
+package by.matrosov.crocoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrocoProjectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/by/matrosov/crocoproject/CrocoProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package by.matrosov.crocoproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrocoProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
