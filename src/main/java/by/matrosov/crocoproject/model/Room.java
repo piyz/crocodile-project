925e73015f963cf9076c5c66d08f0be257d96e72
Andrey Matrosov <matrosovs2008@yandex.ru>
2019-01-30 01:04:53 +0300

    fix generation type

--- src/main/java/by/matrosov/crocoproject/model/Room.java
+++ src/main/java/by/matrosov/crocoproject/model/Room.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Room {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "room_id")
     private long roomId;
 
